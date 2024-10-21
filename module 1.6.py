3c222e55d20c822cc55e2c8f86baa047866926ba
skamshort <andreev16012004@gmail.com>
2024-10-21 14:49:01 +0700

    Add files via upload

---
+++ module 1.6.py
@@ -0,0 +1,16 @@
+my_dict = {'Alex': 2002 , 'Max' : 1995}
+print(my_dict)
+my_dict ['Max'] = 2003
+print(my_dict)
+my_dict.update({'Andrey': 2007,
+                'Joy' : 2011})
+print(my_dict)
+del my_dict ['Joy']
+print(my_dict)
+my_set = {1,2,3,4,1,3,4}
+print(my_set)
+print(my_set.discard(1))
+print(my_set)
+print(my_set.add(7))
+print(my_set.add(9))
+print(my_set)
